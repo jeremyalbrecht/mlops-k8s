d00c790e4d18cf5a39db809aeec68588ae2f66f1
Jérémy Albrecht <me@jalbrecht.fr>
2026-05-14 12:30:07 +0200

    enable ingress addon + YAML config

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
-minikube start --interactive=false --kubernetes-version=v1.35.1 --cpus=4 --memory=8192 --disk-size=30g
+minikube start --interactive=false --kubernetes-version=v1.35.1 --cpus=4 --memory=8192 --disk-size=30g --addons=ingress
 k create ns argocd
 k apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/refs/tags/stable/manifests/install.yaml --server-side
 k wait --for=condition=available deployment/argocd-applicationset-controller -n argocd --timeout=90s
