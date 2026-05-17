9e1b0edd414391867bf5789694696052a856770a
Jérémy Albrecht <me@jalbrecht.fr>
2026-05-17 11:33:27 +0200

    update input CSV paths to use demo bucket

--- worker/worker.py
+++ worker/worker.py
@@ -74,7 +74,7 @@ def mocked_inference(df: pd.DataFrame, model_name: str, batch_size: int) -> np.n
 
 
 def run() -> None:
-    if random.rangrange(0,10) < 5:
+    if random.randrange(0,10) < 5:
         print("Simulating delay in execution to trigger SLA and Deadline alerts")
         time.sleep(60)
     config = load_config(os.environ.get("JOB_CONFIG_PATH", "/app/config/job_config.yaml"))
